dc7d8328c4ae974ae91bb01727adef5e34486bfb
Vince <vince@krijnsen.com>
2024-05-16 10:12:08 +0100

    change duration to take minutes instead of short-medium-long

--- speak_to_data/presentation/forms/forms.py
+++ speak_to_data/presentation/forms/forms.py
@@ -1,7 +1,5 @@
-from speak_to_data import application
 from speak_to_data.presentation.forms import action_form, shared_fields
-from wtforms import RadioField, StringField
-from wtforms.validators import AnyOf
+from wtforms import StringField, IntegerField
 
 
 class QueryForm(action_form.ActionForm):
@@ -18,20 +16,7 @@ class SowForm(action_form.ActionForm):
 
 class MaintainForm(action_form.ActionForm):
     date = shared_fields.date
-    duration = RadioField(
-        validate_choice=False,
-        validators=[
-            AnyOf(
-                values=("1", "3", "6"),
-                message=f"Not a valid choice.\n{application.config.NOT_SAVED_WARNING}",
-            )
-        ],
-        choices=(
-            ("1", "Less than 30 minutes"),
-            ("3", "Between 30 and 90 minutes"),
-            ("6", "More than 90 minutes"),
-        ),
-    )
+    duration = IntegerField("Duration in minutes")
     location = shared_fields.location
     location_type = shared_fields.location_type
 
